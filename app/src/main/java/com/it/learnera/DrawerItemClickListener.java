77f39fee5989ae6cadcffa3a6a74b86f0e221567
Prejith Premkumar <prajithpremg@gmail.com>
2016-06-22 15:52:21 +0300

    added initial login screen placeholders

--- app/src/main/java/com/it/learnera/DrawerItemClickListener.java
+++ app/src/main/java/com/it/learnera/DrawerItemClickListener.java
@@ -5,10 +5,11 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 /**
- * Created by Toshiba on 16-Apr-16.
+ * Created by Prejith on 16-Apr-16.
  */
-public class DrawerItemClickListener implements ListView.OnItemClickListener {
+class DrawerItemClickListener implements ListView.OnItemClickListener {
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // TODO: 22/06/2016  Implementation for item clicks in navigation drawer
     }
 }
--- app/src/main/java/com/it/learnera/MainActivity.java
+++ app/src/main/java/com/it/learnera/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private String[] mDrawerArray;
     private DrawerLayout mDrawerLayout;
